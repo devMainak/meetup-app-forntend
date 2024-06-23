36b01aee4521fc6feb1602c73396831267609e14
rootKid <137388933+devMainak@users.noreply.github.com>
2024-06-23 06:54:41 +0000

    feat: meetup details component

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import MeetUpDetails from './pages/MeetUpDetails'
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App/>
+	},
+	{
+		path: "/:meetUpId",
+		element: <MeetUpDetails/>
 	}
 ])
 
---
+++ src/pages/MeetUpDetails.jsx
@@ -0,0 +1,5 @@
+const MeetUpDetails = () => {
+  
+}
+
+export default MeetUpDetails
